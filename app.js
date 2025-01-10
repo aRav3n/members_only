3fa283ee69bae57d22352ad02eebdb35fc1aab6a
aRav3n <24660772+aRav3n@users.noreply.github.com>
2025-01-10 10:27:16 -0300

    build: set up file structure

---
+++ app.js
@@ -0,0 +1,18 @@
+// Folders: controllers routes views db public
+// npm init -y
+// npm install express ejs express-validator dotenv pg
+
+const express = require("express");
+const app = express();
+const router = require("./routes/router");
+const path = require("node:path");
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", router);
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Express app listening on port PORT!`));
